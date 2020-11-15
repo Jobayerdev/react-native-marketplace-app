51e9722bc30dac6072b249f17e017784dde14fb7
Jobayer Hossain <jobayerhossain977@Gmail.com>
2020-11-15 22:26:02 +0600

    Building the Card Component

--- App.js
+++ App.js
@@ -4,10 +4,23 @@ import ViewImageScreen from "./app/screens/ViewImageScreen"
 import WelcomeScreen from "./app/screens/WelcomeScreen"
 import { MaterialCommunityIcons } from "@expo/vector-icons"
 import AppButton from "./app/components/Button"
+import Card from "./app/components/Card"
+
 const App = () => {
-	return <WelcomeScreen />
+	return (
+		<View
+			style={{
+				backgroundColor: "#f8f4f",
+				padding: 20,
+				paddingTop: 100,
+			}}>
+			<Card
+				title="Red Jacket for sale "
+				subTitle="$100"
+				image={require("./app/assets/jacket.jpg")}
+			/>
+		</View>
+	)
 }
 
 export default App
-
-const styles = StyleSheet.create({})
