6cb2d6b9fe1d0036fe4e6b47778b1b5f7dcf1892
Jobayer Hossain <jobayerhossain977@Gmail.com>
2020-11-29 23:00:41 +0600

    Showing Picker Items

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react"
-import { StyleSheet, Text, TextInput, View } from "react-native"
+import { StyleSheet, Switch, Text, TextInput, View } from "react-native"
 import ViewImageScreen from "./app/screens/ViewImageScreen"
 import WelcomeScreen from "./app/screens/WelcomeScreen"
 import { MaterialCommunityIcons } from "@expo/vector-icons"
@@ -13,12 +13,30 @@ import ListItem from "./app/components/ListItem"
 import AccountScreen from "./app/screens/AccountScreen"
 import ListingsScreen from "./app/screens/ListingsScreen"
 import AppTextInput from "./app/components/AppTextInput"
+import AppPickerInput from "./app/components/AppPicker"
+
+const categories = [
+	{
+		label: "Furniture",
+		value: 1,
+	},
+	{
+		label: "Clothing",
+		value: 2,
+	},
+	{
+		label: "Cameras",
+		value: 3,
+	},
+]
 
 const App = () => {
+	const [isNew, setIsNew] = useState(false)
 	return (
 		<Screen>
 			<View>
-				<AppTextInput icon="mail" placeholder="Username" />
+				<AppPickerInput items={categories} placeholder="Category" icon="apps" />
+				<AppTextInput placeholder="Email" icon="email" />
 			</View>
 		</Screen>
 	)
